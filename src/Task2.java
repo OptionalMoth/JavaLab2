8e5d5aac78a96c8a83846ecca6691ac3ce96064e
Solomon <ksolo15mtetwa@gmail.com>
2023-10-19 00:56:39 +0200

    Use of the Scanner Class to get an input from the user

--- src/Task2.java
+++ src/Task2.java
@@ -1,27 +1,31 @@
-
+import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         //float marsWeightSolo;
-        double Earth = 0.3799999879518072;
-        double Earth1 = 0.37999;
+        //double Earth1 = ;
+        //double Earth1 = 0.3799;
         //char MassEarth = x
-        float Mass = 0;
-        double Mars = Mass/Earth;
-
         System.out.println("Welcome To the Mars Weight Calculator");
+        System.out.println("What is your Mass");
+        Scanner in = new Scanner(System.in);
+        float a = in.nextFloat();
+        float Mars = (float) (a*0.38);
 
 
 
-        System.out.println("Welcome To the Mars Weight Calculator:"+ Mass);
-        System.out.println("Welcome To the Mars Weight Calculator:"+ Mars);
-        System.out.println(Earth1+"kg on Earth is ");
+
+
+
+        //double Mars = Mars/Earth;
+
+
+        System.out.println(a+"kg on Earth is "+Mars+"kg on Mars");
         System.out.println("Kilogram on Mars after converting to double");
         System.out.println("Kilograms on Mars displayed to four decimal places:");
         System.out.println("Kilograms when casted to integer ");
         System.out.println("The ASCII table equivalent  of marsWeightInInt is");
         System.out.println("An example of a mathematical operation on the char type:");
-
-
+        System.out.println("You entered integer " + a);
 
     }
 }
