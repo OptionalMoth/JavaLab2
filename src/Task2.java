9d1805add988b76232844ecfb13aa6fe4c768cd6
Solomon <ksolo15mtetwa@gmail.com>
2023-10-19 01:21:01 +0200

    Converting Output from float to double

--- src/Task2.java
+++ src/Task2.java
@@ -1,27 +1,20 @@
 import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
-        //float marsWeightSolo;
-        //double Earth1 = ;
-        //double Earth1 = 0.3799;
-        //char MassEarth = x
+
         System.out.println("Welcome To the Mars Weight Calculator");
         System.out.println("What is your Mass");
         Scanner in = new Scanner(System.in);
         float a = in.nextFloat();
         float Mars = (float) (a*0.38);
+        double b = Mars;
+        double c = Integer.MIN_VALUE + 0.1000 + b;
 
-
-
-
-
-
-        //double Mars = Mars/Earth;
 
 
         System.out.println(a+"kg on Earth is "+Mars+"kg on Mars");
-        System.out.println("Kilogram on Mars after converting to double");
-        System.out.println("Kilograms on Mars displayed to four decimal places:");
+        System.out.println("Kilogram on Mars after converting to double"+b);
+        System.out.println("Kilograms on Mars displayed to four decimal places:"+c);
         System.out.println("Kilograms when casted to integer ");
         System.out.println("The ASCII table equivalent  of marsWeightInInt is");
         System.out.println("An example of a mathematical operation on the char type:");
