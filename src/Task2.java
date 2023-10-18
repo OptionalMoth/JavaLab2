9567c594f61fe17a317624f86cc0003a2da1ccbf
Solomon <ksolo15mtetwa@gmail.com>
2023-10-19 00:18:59 +0200

    Initial commit

---
+++ src/Task2.java
@@ -0,0 +1,27 @@
+
+public class Task2 {
+    public static void main(String[] args) {
+        //float marsWeightSolo;
+        double Earth = 0.3799999879518072;
+        double Earth1 = 0.37999;
+        //char MassEarth = x
+        float Mass = 0;
+        double Mars = Mass/Earth;
+
+        System.out.println("Welcome To the Mars Weight Calculator");
+
+
+
+        System.out.println("Welcome To the Mars Weight Calculator:"+ Mass);
+        System.out.println("Welcome To the Mars Weight Calculator:"+ Mars);
+        System.out.println(Earth1+"kg on Earth is ");
+        System.out.println("Kilogram on Mars after converting to double");
+        System.out.println("Kilograms on Mars displayed to four decimal places:");
+        System.out.println("Kilograms when casted to integer ");
+        System.out.println("The ASCII table equivalent  of marsWeightInInt is");
+        System.out.println("An example of a mathematical operation on the char type:");
+
+
+
+    }
+}
