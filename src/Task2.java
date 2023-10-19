982257c0d80856f0b13446d96fc9987aadb6c52e
Solomon <ksolo15mtetwa@gmail.com>
2023-10-19 03:08:26 +0200

    Converting Output from Double to int and from int to Double

--- src/Task2.java
+++ src/Task2.java
@@ -1,24 +1,34 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
 
+        double b;
+        double c;
+        int d;
+        char e;
+        DecimalFormat numberFormat = new DecimalFormat("#.####");
         System.out.println("Welcome To the Mars Weight Calculator");
         System.out.println("What is your Mass");
         Scanner in = new Scanner(System.in);
         float a = in.nextFloat();
         float Mars = (float) (a*0.38);
-        double b = Mars;
-        double c = Integer.MIN_VALUE + 0.1000 + b;
-
+        b = Mars;
+        c = b;
+        d = (int) c;
+        e = (char)d;
 
 
         System.out.println(a+"kg on Earth is "+Mars+"kg on Mars");
-        System.out.println("Kilogram on Mars after converting to double"+b);
+        System.out.println("Kilogram on Mars after converting to double "+b);
         System.out.println("Kilograms on Mars displayed to four decimal places:"+c);
-        System.out.println("Kilograms when casted to integer ");
-        System.out.println("The ASCII table equivalent  of marsWeightInInt is");
+
+        //System.out.printf("%.3f", c);
+        //System.out.println(numberFormat.format(c));
+
+        System.out.println("Kilograms when casted to integer "+d);
+        System.out.println("The ASCII table equivalent  of marsWeightInInt is "+e);
         System.out.println("An example of a mathematical operation on the char type:");
-        System.out.println("You entered integer " + a);
 
     }
 }
